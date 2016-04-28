4dd279d4dc3d0d34506da3b540b53a83d4ac79f3
Eiichiro Iwata <iwata@preferred.jp>
2016-04-28 14:07:51 +0900

    Fixed .zshrc to install google-c-style and use it when editing C/C++ source files.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -56,4 +56,17 @@
 
 ;;;;; Config for google-c-style
 (add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c++-mode-common-hook 'google-set-c-style)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+
+;; function decides whether .h file is C or C++ header, sets C++ by
+;; default because there's more chance of there being a .h without a
+;; .cc than a .h without a .c (ie. for C++ template files)
+(defun c-c++-header ()
+  "sets either c-mode or c++-mode, whichever is appropriate for header"
+  (interactive)
+  (let ((c-file (concat (substring (buffer-file-name) 0 -1) "c")))
+    (if (file-exists-p c-file)
+	(c-mode)
+      (c++-mode))))
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c-c++-header))
